c5f7624f5aaf8c5b0c43942d437daf23a5ff1fa4
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2020-03-23 18:42:08 +0900

    add test

--- src/foo.ts
+++ src/foo.ts
@@ -1,3 +1,11 @@
 export const sum = (...args: number[]) => {
   return args.reduce((acc, value) => acc + value, 0);
 };
+
+export const APIRequest = (who: string) => {
+  if (who === "google") {
+    return fetch("https://google.com").then(res => res.json());
+  } else {
+    return Promise.resolve("no argument provided");
+  }
+};
--- src/mock.test.ts
+++ src/mock.test.ts
@@ -1,8 +1,6 @@
-import {enableFetchMocks} from "jest-fetch-mock";
-import { sum } from "./foo";
-
+import {APIRequest, sum} from "./foo";
+import fetchMock, {enableFetchMocks} from "jest-fetch-mock";
 enableFetchMocks();
-
 
 
 test("1 + 2", () => {
@@ -15,10 +13,27 @@ test("basic", async () => {
 
 test("basic again", async () => {
   expect(sum(1, 2)).toBe(3);
-}, 1000);
+});
+
+describe("testing api", () => {
+  beforeEach(() => {
+    fetchMock.resetMocks()
+  })
+
+  it("calls google and returns data to me", () => {
+    fetchMock.mockResponseOnce(JSON.stringify({ data: "123456"}))
+
+    APIRequest("google").then(res => {
+      expect(res.data).toEqual("123456")
+    })
+
+    expect(fetchMock.mock.calls.length).toEqual(1)
+    expect(fetchMock.mock.calls[0][0]).toEqual("https://google.com")
+  })
+})
 
 // it('should beforeEach make mock', function () {
-//   fetchMock.mockIf(/^https:?:\/\/example.com.*$/, req => {
+//   fetchMockMock.mockIf(/^https:?:\/\/example.com.*$/, req => {
 //     if (req.url.endsWith("/path1")) {
 //       return "some response body"
 //     } else if (req.url.endsWith("/path2")) {
