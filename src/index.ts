936066ecd2d9db783bf60361be712b8c9d3ad10a
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2020-03-23 12:23:04 +0900

    add example of async/await

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log("Hello");
+
+function asyncFunc(params: number): Promise<number> {
+  return new Promise(resolve =>  {
+    setTimeout(() => {
+      resolve(params);
+    }, 2000)
+    }
+  )
+}
+
+async function sample() {
+  const result = await asyncFunc(3);
+  return result + 5;
+}
+
+sample().then(result => {
+  console.log(result);
+});
+
+async function sum(array: number[]) {
+  return array.reduce(async (sum, value) => {
+    return await sum + await asyncFunc(value);
+  }, 0);
+}
+
+sum([4, 2]).then(value => {
+  console.assert(typeof value === "number");
+  console.log(value);
+});
