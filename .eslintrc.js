e02c22053f87d5fd7be9eab7fc77d16b78b1a07a
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2020-04-07 15:33:40 +0900

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,27 @@
 module.exports = {
-  extends: ['airbnb-typescript'],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint"
+  ],
+  plugins: [
+    "@typescript-eslint"
+  ],
+  parser: "@typescript-eslint/parser",
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
   parserOptions: {
     project: './tsconfig.json',
+  },
+  rules: {
+    eqeqeq: "warn",
+    curly: "warn",
+    quotes: ["error", "double"],
+    semi: ["error", "always"]
   },
 };
