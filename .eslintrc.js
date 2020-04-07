6e1bdbc467ceb92b7ff18e8cfd94ec3811a675f4
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2020-04-07 17:19:32 +0900

    it can run

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,65 @@
+// module.exports = {
+//   extends: [
+//     "eslint:recommended",
+//     "plugin:@typescript-eslint/recommended",
+//     "plugin:@typescript-eslint/eslint-recommended",
+//     "plugin:prettier/recommended",
+//     "prettier/@typescript-eslint",
+//     "plugin:react/recommended"
+//   ],
+//   plugins: [
+//     "@typescript-eslint"
+//   ],
+//   parser: "@typescript-eslint/parser",
+//   env: {
+//     browser: true,
+//     node: true,
+//     es6: true,
+//   },
+//   rules: {
+//     eqeqeq: "warn",
+//     curly: "warn",
+//     quotes: ["error", "double"],
+//     semi: ["error", "always"]
+//   },
+// };
 module.exports = {
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:prettier/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:react/recommended"
-  ],
+  parser: '@typescript-eslint/parser',
   plugins: [
-    "@typescript-eslint"
+    '@typescript-eslint',
+    'react',
+    'react-hooks'
   ],
-  parser: "@typescript-eslint/parser",
   env: {
     browser: true,
-    node: true,
-    es6: true,
+    jest: true
   },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
   parserOptions: {
-    project: './tsconfig.json',
+    project: [
+      "tsconfig.json",
+    ],
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
   },
   rules: {
-    eqeqeq: "warn",
-    curly: "warn",
-    quotes: ["error", "double"],
-    semi: ["error", "always"]
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react/jsx-filename-extension': ['warn', {extensions: ['.jsx', '.tsx']}],
+    'react/prop-types': 'off',
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
   },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
 };
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <>
+      <a
+        href="https://r4---sn-oguesnzd.googlevideo.com/videoplayback?expire=1586267914&ei=qjKMXqNK1puBA66-jpAG&ip=219.111.52.120&id=o-ACs3EfV8MZdpmWP6Fm194hNqI8kpC7Ppo9q7f6dx-EJm&itag=140&source=youtube&requiressl=yes&mh=Yk&mm=31%2C26&mn=sn-oguesnzd%2Csn-3pm7kn7e&ms=au%2Conr&mv=m&mvi=3&pl=22&initcwndbps=1196250&vprv=1&mime=audio%2Fmp4&gir=yes&clen=1515917&dur=93.622&lmt=1586245322091105&mt=1586246140&fvip=4&keepalive=yes&c=WEB&txp=5431432&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJpPlLswRQIhAI_qlncl9DTqWVjWE0yuxZjnlAlGn8SHZvTvhteJ9ZBzAiA6gIUPgmax0ul6iCgjAwbn64WwSg01d_ECvkqLIG19Rw%3D%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=ALrAebAwRAIgYKoQLBn3EF1703kNxn7DEYQmEGBKrAXguwaPhK1XkqQCIB4OZ1kM76o44DGBibkH5ueTGmuhC9TzODtvALy-fFSs&ratebypass=yes"
+        download="video.m4a"
+        ><button>hoge</button></a>
+      <div>Hello, world</div>
+    </>
+  );
+};
